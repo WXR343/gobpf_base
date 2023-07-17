f0cc8e63fd5e32e1a45732b4a335b21c763f73f2
WXR343 <111735703+WXR343@users.noreply.github.com>
2023-07-17 19:48:27 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main(){<!-- -->
+fmt.Println("Please enter your firstname and lastname")
+var a1,a2 string
+fmt.Scan(&a1,&a2)
+fmt.Println("hello ",a1," and ",a2)
+}
